f47b37df15671fb12d151bb5a1ad5ac6cc954976
mariamattef <mariamatte656@gmail.com>
2025-02-12 13:10:10 +0200

    add user params

--- lib/features/user/domain/repositories/user_repository.dart
+++ lib/features/user/domain/repositories/user_repository.dart
@@ -1,7 +1,8 @@
+import 'package:clean_arch/core/params/params.dart';
 import 'package:clean_arch/features/user/domain/entities/user_intity.dart';
 import 'package:dartz/dartz.dart';
 import '../../../../core/errors/failure.dart';
 
 abstract class UserRepository {
-  Future<Either<Failure,UserIntity>> getUsers();
+  Future<Either<Failure,UserIntity>> getUsers(UserParams params);
 }
---
+++ lib/features/user/domain/use_cases/get_user_usecase.dart
@@ -0,0 +1,15 @@
+import 'package:clean_arch/core/params/params.dart';
+import 'package:clean_arch/features/user/domain/entities/user_intity.dart';
+import 'package:clean_arch/features/user/domain/repositories/user_repository.dart';
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failure.dart';
+
+class GetUserUsecase {
+  final UserRepository _userRepository;
+
+  GetUserUsecase(this._userRepository);
+ Future<Either<Failure, UserIntity>> call(UserParams params) {
+    return _userRepository.getUsers(params);
+  }
+}
